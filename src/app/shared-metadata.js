c8638aea943b7bde4cb8c56d1e80ecfd035538ce
Valentino Giardino <valentinogiardino17@gmail.com>
2024-03-10 17:54:17 -0300

    v1

--- src/app/shared-metadata.js
+++ src/app/shared-metadata.js
@@ -1,5 +1,5 @@
-export const sharedTitle = 'Onur Şuyalçınkaya'
-export const sharedDescription = 'Software Engineer, DJ, writer, and minimalist, based in Amsterdam, The Netherlands'
+export const sharedTitle = 'Valentino Giardino'
+export const sharedDescription = 'Software Engineer, based in Salta, Argentina'
 export const sharedImage = {
   width: 1200,
   height: 630,
